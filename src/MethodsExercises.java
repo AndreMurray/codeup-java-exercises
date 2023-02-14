662958c287116410e7badd0a38ee14b0413bf35f
Andre Murray <slushymatis@gmail.com>
2023-02-13 22:51:30 -0600

    recursive min and max done

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -18,14 +18,21 @@ public class MethodsExercises {
     public static int division(int num1, int num2) {
         return num1 / num2;
     }
-
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        int userInt = sc.nextInt();
+        if (userInt >= min && userInt <= max) {
+            return userInt;
+        } else {
+            System.out.println("Input out of range!");
+            return getInteger(min, max);
+        }
+    }
 
     public static void main(String[] args) {
 
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter an integer.");
-       String interger = sc.nextLine();
-
+    System.out.print("Enter a number between 1 and 10:");
+    int userInput = getInteger(1, 10);
 
 
     }
